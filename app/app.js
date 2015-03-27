2b635c85253538399d125a92f5868c87bc302432
Fabien O'Carroll <fabien@allou.is>
2015-03-27 16:02:43 +0000

    better??

--- app/app.js
+++ app/app.js
@@ -1,14 +1,19 @@
 import { createBoard } from 'components/board';
-import { set, getSymbol, create } from 'board';
+import { set, get, getSymbol, create } from 'board';
 import { addEventOnce, getIndex } from 'utils';
 import React from 'react';
 
 export function render(board, elem){
     React.render(createBoard(board), elem);
-    addEventOnce(elem, 'click', function (event){
-        if (!get(getIndex(event.target), board)){
-            return render(set(getSymbol(board), getIndex(event.target), board), elem);
+    addEvents(board, elem);
+}
+
+function addEvents(board, elem){
+    addEventOnce(elem, 'click', function onClick(event){
+        if (get(getIndex(event.target), board)) {
+            return addEventOnce(elem, 'click', onClick);
         }
+        return render(set(getSymbol(board), getIndex(event.target), board), elem);
     });
 }
 
